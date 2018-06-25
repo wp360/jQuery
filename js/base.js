c3efdae480d20894d564faf008b9fbf3d59b7e80
wp360 <2897406527@qq.com>
2018-06-26 01:56:37 +0800

    自定义弹框

--- js/base.js
+++ js/base.js
@@ -4,6 +4,7 @@
     // store.set('user','bob');
     // var user = store.get('user');
     // console.log(user);
+    var $body = $('body');
     var $form_add_task = $('.add-task');
     var task_list = [];
     var $task_delete;
@@ -139,13 +140,17 @@
             var index = $item.data('index');
             // task_delete(index);
             // 确认删除
-            var tmp = confirm('确定删除？');
+            my_alert('确定删除？')
+                .then(function(result){
+                    result ? task_delete(index) : null;
+                });
+            // var tmp = confirm('确定删除？');
             // tmp ? task_delete(index) : null;
-            if(tmp === true){
-                task_delete(index);
-            }else{
-                return;
-            }
+            // if(tmp === true){
+            //     task_delete(index);
+            // }else{
+            //     return;
+            // }
             // console.log('index', $item.data('index'));
         });
     }
@@ -295,6 +300,136 @@
 
     function hide_msg() {
         $msg.hide();
+    }
+
+    // my_alert('确定要删除此项任务吗？').then(function(result){
+    //     console.log('result',result);
+    // });
+
+    // 弹框
+    function my_alert(arg){
+        if(!arg) {
+            console.error('pop title is required');
+        }
+        var conf = {},
+        $box,
+        $mask,
+        $title,
+        $content,
+        $confirm,
+        $cancel,
+        dfd,
+        timer,
+        confirmed;
+
+        dfd = $.Deferred();
+
+        if (typeof arg == 'string') {
+            conf.title = arg;
+        } else {
+            conf = $.extend(conf, arg);
+        }
+
+        $box = $('<div><div class="pop-title">' + conf.title + '</div>' +
+        '<div class="pop-content">' +
+        '<div><button type="button" class="primary confirm">确定</button><button type="button" class="cancel">取消</button></div>' +
+        '</div>' +
+        '</div>')
+        .css({
+            position: 'fixed',
+            color: '#333',
+            width: 300,
+            height: 'auto',
+            padding: '30px 0',
+            background: '#fff',
+            'border-radius': 3,
+            'box-shadow': '0 1px 2px rgba(0,0,0,.5)'
+        });
+
+        $title = $box.find('.pop-title').css({
+            padding: '5px 10px',
+            'font-size': '20px',
+            'font-weight': 900,
+            'text-align': 'center'
+        });
+
+        $content = $box.find('.pop-content').css({
+            padding: '5px 10px',
+            'text-align': 'center'
+        });
+
+        $mask = $('<div></div>').css({
+            position: 'fixed',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: 'rgba(0,0,0,.5)'
+        });
+        $confirm = $content.find('button.confirm');
+        $cancel = $content.find('button.cancel');
+
+        // $confirm.on('click',function(){
+        //     console.log('确定');
+        //     confirmed = true;
+        // });
+
+        // $cancel.on('click', function () {
+        //     console.log('取消');
+        //     confirmed = false;
+        // });
+
+        $confirm.on('click', on_confirm);
+        $cancel.on('click', on_cancel);
+        $mask.on('click', on_cancel);
+
+        function on_confirm() {
+            confirmed = true;
+        }
+
+        function on_cancel() {
+            confirmed = false;
+        }
+
+        timer = setInterval(function () {
+            if (confirmed !== undefined) {
+                dfd.resolve(confirmed);
+                clearInterval(timer);
+                dismiss_alert();
+            }
+        }, 50);
+
+        function dismiss_alert(){
+            $mask.remove();
+            $box.remove();
+        }
+
+        function adjust_box_position() {
+            var window_width = $(window).width();
+            var window_height = $(window).height();
+            var box_width = $box.width();
+            var box_height = $box.height();
+            var move_x, move_y;
+
+            move_x = (window_width - box_width) / 2;
+            move_y = ((window_height - box_height) / 2) - 20;
+
+            $box.css({
+                left: move_x,
+                top: move_y
+            });
+        }
+
+        $(window).on('resize',function(){
+            adjust_box_position();
+        });
+
+        adjust_box_position();
+
+        $mask.appendTo($body);
+        $box.appendTo($body);
+        $(window).resize();
+        return dfd.promise();
     }
 
     // 初始化
